b8951e36d9a8306ba2db29b64d26feb0e0370d86
Anshika Kataria <148747427+anshika1093@users.noreply.github.com>
2024-09-11 19:02:42 +0530

    Add files via upload

--- ForgotPassword.jsx
+++ ForgotPassword.jsx
@@ -4,6 +4,7 @@ import salonx from '../Salon-x.png';
 import bg from '../Rectangle 34624655.png';
 import { IoArrowBackSharp } from "react-icons/io5";
 import { useNavigate } from 'react-router-dom';
+import lock from '../lock-icon1.png'
 
 function ForgotPassword() {
   const [email, setEmail] = useState('');
@@ -11,8 +12,9 @@ function ForgotPassword() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Implement your password reset logic here
+    
     console.log('Email:', email);
+    navigate('/resetpassword')
   };
 
   const handleGoBack = () => {
@@ -20,7 +22,7 @@ function ForgotPassword() {
   };
 
   return (
-    <div className="wrapper">
+    <div className="wrapper1">
       <div className="salon">
         <a href="#"><img src={salonx} alt="Salon-X"></img> Salon-X</a>
         <a href="#" className="previous" onClick={handleGoBack}><IoArrowBackSharp /></a>
@@ -28,10 +30,11 @@ function ForgotPassword() {
       
       <div className="form-box-forgotpassword">
         <form onSubmit={handleSubmit}>
+          <img src={lock}></img>
           <h2>Forgot Password?</h2>
-          <p>Enter your email address and we'll send you a link to reset your password.</p><br></br>
+          <p>All good. Enter your account's email address and we’ll send you a <br></br>link to reset your password.</p><br></br>
           
-          <div className="input-box">
+          <div className="input-box-reset">
             <input
               type="email"
               placeholder="Email Address"
